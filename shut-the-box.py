b0502c14408810b8f59cefe7ca4720c49dec1ff5
eeshamoona <emoona2@illinois.edu>
2020-05-11 18:10:50 -0700

    Printed out the user's dice roll

--- shut-the-box.py
+++ shut-the-box.py
@@ -3,4 +3,10 @@ import random
 def getRandomDiceRoll():
     return random.randint(1,7)
 
+openList = [1,2,3,4,5,6,7,8,9,10,11,12]
+closeList = ["_","_","_","_","_","_","_","_","_","_","_","_"]
+diceOne = getRandomDiceRoll()
+diceTwo = getRandomDiceRoll()
+
+print("Your dice rolled a " + str(diceOne) + " and " + str(diceTwo))
 
